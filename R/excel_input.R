db290b3e5a944d2e04cc658c8607ff6bba93e732
Peter Curtis <peter.curtis@education.gov.uk>
2018-05-22 23:13:56 +0100

    Created functions that pull all specific inputs from the excel sheet

--- R/excel_input.R
+++ R/excel_input.R
@@ -1,13 +1,46 @@
-library(tidyverse)
-input <- readxl::read_excel("Excel input.xlsx", sheet = "Inputs")
-repair_funds <- input %>% select(`Repair budget`) %>% as.numeric
-rebuild_funds <- input %>% select(`Rebuild budget`) %>% as.numeric
-inflation <- input %>% select(Inflation) %>% as.numeric
-years <- names(input)[7] %>% as.numeric
-block_rebuild_cost <- input[1, 7] %>% as.numeric
-rebuild_order <- input[2, 7] %>% pull() %>% strsplit("") %>% unlist()
-location_factor <- input[3, 7] %>% as.logical
-save_path <- input[5, 7]
-save_label <- input[6, 7]
+load_forecast_horizon <- function(path = "./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G1", col_names = "forecast_horizon") %>% pull()
+}
 
+load_block_unit_rebuild <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G2", col_names = "unit_rebuild_cost") %>% pull()
+}
 
+load_grade_order <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G3", col_names = "grade_order") %>% pull() %>%
+    strsplit("") %>% unlist()
+}
+
+load_location_factor <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G4", col_names = "location_factor") %>% pull()
+}
+
+load_save_path <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G6", col_names = "save_path") %>% pull()
+}
+
+load_file_label <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Inputs!G7", col_names = "file_label") %>% pull()
+}
+
+load_repair_budget <- function(path ="./inst/Excel input.xlsx", years){
+  read_excel(path, range = paste0("Inputs!B2:B", years + 1), col_names = "repair_budget") %>% pull()
+}
+
+load_rebuild_budget <- function(path ="./inst/Excel input.xlsx", years){
+  read_excel(path, range = paste0("Inputs!C2:C", years + 1), col_names = "rebuild_budget") %>% pull()
+}
+
+load_inflation <- function(path ="./inst/Excel input.xlsx", years){
+  read_excel(path, range = paste0("Inputs!D2:D", years + 1), col_names = "inflation") %>% pull()
+}
+
+load_det_rates <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Deterioration rates!A1:E129") %>%
+    rename(elementid = "Element ID")
+}
+
+load_repair_costs <- function(path ="./inst/Excel input.xlsx"){
+  read_excel(path, range = "Repair costs!A1:E129") %>%
+    rename(elementid = "Element ID")
+}
